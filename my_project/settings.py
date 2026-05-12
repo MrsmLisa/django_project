5d36d3b3172011753d8d314150c708e00620e606
mrsmlisa <lisa.q.scott@gmail.com>
2026-05-12 11:06:08 +0200

    trying to add heroku app but it is wrong somewhere

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8&hd!o9mwq9(z4py-&7$_wl-gz6clwfibpvnqpf17um=9)t0=6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
